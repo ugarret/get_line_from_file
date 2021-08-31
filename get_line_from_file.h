ef0bdec1849c66b68086278d386c609316813e2f
ugarret <76210825+ugarret@users.noreply.github.com>
2021-09-01 02:29:47 +0300

    Update get_line_from_file.h

--- get_line_from_file.h
+++ get_line_from_file.h
@@ -18,12 +18,12 @@
 #  define BUFFER_SIZE 32
 # endif
 
-int		get_next_line(int fd, char **line);
-int		ft_strlen(const char *str);
-char	*ft_substring(const char *str, int start, int len);
-char	*ft_strjoin(char *str1, char *str2);
-int		ft_search_index_newline(const char *str);
-char	*ft_memory_for_new_string(int i);
-int		write_in_remainder_and_line(char **rem_line, char **line);
+int	  	get_next_line(int fd, char **line);
+int 		ft_strlen(const char *str);
+char	  *ft_substring(const char *str, int start, int len);
+char	  *ft_strjoin(char *str1, char *str2);
+int		  ft_search_index_newline(const char *str);
+char	  *ft_memory_for_new_string(int i);
+int		  write_in_remainder_and_line(char **rem_line, char **line);
 
 #endif
