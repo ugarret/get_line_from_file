bf65886b5e0e38b8d0a81a3f027aec63d1b8ec45
ugarret <76210825+ugarret@users.noreply.github.com>
2021-09-01 02:27:27 +0300

    Small changes
    Small changes, that do not affect the work of the code

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ugarret <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/12 17:28:12 by ugarret           #+#    #+#             */
-/*   Updated: 2020/12/12 19:13:16 by ugarret          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-
 #include <fcntl.h>
 #include <stdio.h>
-#include "get_next_line.h"
+#include "get_line_from_file.h"
 
 int		main()
 {
-	int		fd;
+	int	fd;
 	char	*line;
-	int		i;
+	int	i;
 
 	fd = open("64bit_paragraph.txt", O_RDONLY);
-	while ((i = get_next_line(fd, &line)) > 0)
+	while ((i = get_line_from_file(fd, &line)) > 0)
 	{
 		printf("i = %d %s\n", i, line);
 		free(line);
